f5b649b9fd02571ead9706adf4073f6ec4e9589a
PlayerG9 <g9yout@gmail.com>
2023-07-06 10:36:40 +0200

    added get_openapi_spec

--- src/dynapi/routes/meta/list_columns.py
+++ src/dynapi/routes/meta/list_columns.py
@@ -29,3 +29,67 @@ def columns(schema: str, tablename: str):
             )
             for row in cursor.fetchall()
         ])
+
+
+def get_openapi_spec():
+    return {
+        '/list-columns/{schema}/{table}': {
+            'get': {
+                'tags': ["Meta"],
+                'summary': "Gets all columns of a specific table within a schema",
+                'parameters': [
+                    {
+                        'name': "schema",
+                        'in': "path",
+                        'description': "Database Schema",
+                        'schema': dict(
+                            type="string"
+                        )
+                    },
+                    {
+                        'name': "table",
+                        'in': "path",
+                        'description': "Database Table Name",
+                        'schema': dict(
+                            type="string",
+                        ),
+                    },
+                ],
+                'responses': {
+                    '200': {
+                        'description': "Successful operation",
+                        'content': {
+                            "application/json": {
+                                'schema': {
+                                    'type': "array",
+                                    'items': {
+                                        'type': "object",
+                                        'properties': {
+                                            'column_name': dict(
+                                                type="string"
+                                            ),
+                                            'is_nullable': dict(
+                                                type="string"
+                                            ),
+                                            'data_type': dict(
+                                                type="string"
+                                            ),
+                                            'is_identity': dict(
+                                                type="string"
+                                            ),
+                                            'is_generated': dict(
+                                                type="string"
+                                            ),
+                                            'is_updatable': dict(
+                                                type="string"
+                                            ),
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
