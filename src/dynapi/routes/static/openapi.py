deaecba55257047a045a5d6146568ba6b131174b
PlayerG9 <g9yout@gmail.com>
2023-07-06 10:35:24 +0200

    added openapi paths loading

--- src/dynapi/routes/static/openapi.py
+++ src/dynapi/routes/static/openapi.py
@@ -3,22 +3,31 @@
 r"""
 
 """
-from __main__ import app, __version__
+from __main__ import app, __version__, ROUTES
 
 
 @app.route("/openapi")
 def openapi():
-    # summary, description = __doc__.split('\n\n', 1)
+    paths = {}
+    for route in ROUTES:
+        if not hasattr(route, 'get_openapi_spec'):
+            continue
+        try:
+            spec = route.get_openapi_spec()
+            if not isinstance(spec, dict):
+                raise TypeError(f"{type(spec).__name__} is not of type dict")
+        except Exception as exc:
+            print(f"Failed to load openapi_spec from {route.__qualname__}")
+            print(f"{type(exc).__name__}: {exc}")
+        else:
+            paths.update(spec)
     return dict(
         openapi="3.0.0",
         info=dict(
             title="DynAPI",
             version=__version__,
             # summary=summary,
-            # description=description
             description=__doc__,
         ),
-        paths={
-            # "/": {}
-        },
+        paths=paths,
     )
