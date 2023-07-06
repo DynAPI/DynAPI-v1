cdd23a8535d2a800c18046950b376b22e2d1f353
PlayerG9 <g9yout@gmail.com>
2023-07-06 13:22:00 +0200

    prepared other dynamic endpoints

---
+++ src/dynapi/routes/generated/delete_dynamic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+from __main__ import app
+from exceptions import DoNotImportException
+from apiconfig import config
+
+
+if not config.get("methods", "delete"):
+    raise DoNotImportException()
