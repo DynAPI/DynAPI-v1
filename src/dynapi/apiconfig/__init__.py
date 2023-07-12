d0e098f79c663f16d05a658b1dc9785e22da33bf
PlayerG9 <g9yout@gmail.com>
2023-07-12 11:52:51 +0200

    improved method_check and add flask_method_check() function

--- src/dynapi/apiconfig/__init__.py
+++ src/dynapi/apiconfig/__init__.py
@@ -4,4 +4,4 @@ r"""
 
 """
 from .fileconfig import config
-from .method_check import method_check
+from .method_check import method_check, flask_method_check
--- src/dynapi/apiconfig/method_check.py
+++ src/dynapi/apiconfig/method_check.py
@@ -4,7 +4,10 @@ r"""
 TIP: don't ever touch this stuff, or you will get headaches
 """
 import re
+import http
 import functools
+import typing as t
+import flask
 from .fileconfig import config
 
 
@@ -35,26 +38,27 @@ class ParsedSection:
         if self.section != 'methods':
             raise KeyError(f"invalid section: {self.section!r}")
         self.schema = parts[1] if len(parts) > 1 else None
-        self.schema_re = re.compile(translate_globby(self.schema)) if self.schema else None
+        self.schema_re = re.compile(translate_globby(self.schema), re.IGNORECASE) if self.schema else None
         self.table = parts[2] if len(parts) > 2 else None
-        self.table_re = re.compile(translate_globby(self.table)) if self.table else None
+        self.table_re = re.compile(translate_globby(self.table), re.IGNORECASE) if self.table else None
 
         self.worth = get_worth(self.schema) + get_worth(self.table) * 10
 
     def __repr__(self):
         return f"<{type(self).__name__} {self.schema or ''}:{self.table or ''}>"
 
+    # used for sorting
     def __lt__(self, other: 'ParsedSection'):
         return self.worth < other.worth
 
-    def match_schema(self, schema) -> bool:
+    def match_schema(self, schema) -> t.Optional[bool]:
         if not self.schema:
-            return True
+            return None
         return self.schema_re.fullmatch(schema) is not None
 
-    def match_table(self, table) -> bool:
+    def match_table(self, table) -> t.Optional[bool]:
         if not self.table:
-            return True
+            return None
         return self.table_re.fullmatch(table) is not None
 
 
@@ -69,8 +73,18 @@ def ordered_sections():
 @functools.lru_cache()
 def method_check(*, method: str, schema: str, table: str) -> bool:
     for section in ordered_sections():
-        if section.match_schema(schema) and section.match_table(table):
+        if section.match_schema(schema) is not False and section.match_table(table) is not False:
             allowed = config.getboolean(section.raw, method, fallback=None)
             if allowed is not None:
                 return allowed
     return False
+
+
+# def flask_method_check(*, method: str, schema: str, table: str):
+def flask_method_check(*, method: str = None, schema: str = None, table: str = None):
+    method = method or flask.request.method
+    schema = schema or flask.request.view_args["schemaname"]
+    table = table or flask.request.view_args["tablename"]
+    if not method_check(method=method, schema=schema, table=table):
+        # flask.abort(http.HTTPStatus.FORBIDDEN)
+        flask.abort(http.HTTPStatus.METHOD_NOT_ALLOWED)
