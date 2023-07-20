fe63e0e4eb8d723d69a0c58549b37e3a1edd7de3
PlayerG9 <g9yout@gmail.com>
2023-07-20 14:04:22 +0200

    fixed body.limit and body.offset

--- src/dynapi/apiutil/body_config.py
+++ src/dynapi/apiutil/body_config.py
@@ -11,9 +11,9 @@ from pydantic import dataclasses
 
 @dataclasses.dataclass
 class BodyConfig:
-    limit: int = dataclasses.Field(default=1000, gt=0, le=1000)
-    offset: int = dataclasses.Field(default=0, ge=0)
-    resolve_depth: int = dataclasses.Field(default=0, ge=0, le=10)
+    limit: int = dataclasses.Field(None, gt=0)
+    offset: int = dataclasses.Field(None, ge=0)
+    # resolve_depth: int = dataclasses.Field(default=0, ge=0, le=10)
     columns: t.List[str] = dataclasses.Field(default_factory=lambda: ["*"])
     filters: t.List[t.List[t.Tuple[str, str, t.Union[bool, int, float, str]]]] = dataclasses.Field(default_factory=list)
     obj: dict = dataclasses.Field(None)
