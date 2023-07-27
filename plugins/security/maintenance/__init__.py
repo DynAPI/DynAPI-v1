f8320a0b37a8ee9270af3be1423609536114ac83
PlayerG9 <g9yout@gmail.com>
2023-07-27 08:48:21 +0200

    added new maintenance plugin

---
+++ plugins/security/maintenance/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+r"""
+
+"""
+from __main__ import app, __version__
+import textwrap
+import flask
+from werkzeug.exceptions import ServiceUnavailable
+
+
+@app.before_request
+def maintenance():
+    if flask.request.path.startswith("/api"):
+        raise ServiceUnavailable()
+    if flask.request.path == flask.url_for("openapi"):
+        return openapi_maintenance_response()
+
+
+def openapi_maintenance_response():
+    return dict(
+        openapi="3.0.0",
+        info={
+            'title': "DynAPI",
+            'version': __version__,
+            # summary=summary,
+            'description': textwrap.dedent(fr"""
+                # This DynAPI instance is currently under maintenance and not available
+                """),
+            'x-logo': dict(
+                url=flask.url_for("static", filename="assets/DynAPI.svg"),
+            )
+        },
+    )
