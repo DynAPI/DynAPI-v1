9dd3d2e50fde54e1a9888e1aa65bc8c25c728570
PlayerG9 <g9yout@gmail.com>
2023-08-04 12:15:14 +0200

    added user validation

--- plugins/auth/administration/__init__.py
+++ plugins/auth/administration/__init__.py
@@ -6,9 +6,20 @@ password with constraints
 admin-ping/debug-pin
 """
 from __main__ import app
-import http
 import hmac
+import http
+import base64
 import flask
+from pypika import PostgreSQLQuery as Query, Schema, Table
+from apiconfig import config
+from .util import compare_password_to_hash
+
+
+USERS_SCHEMANAME = config.get('auth', 'schema') if config.has_option('auth', 'schema') else 'dynapi'
+USERS_TABLENAME = config.get('auth', 'users_table') if config.has_option('auth', 'users_table') else 'users'
+
+ADMIN_USERNAME = config.get("plugin:administration", "username")
+ADMIN_PASSWORD = config.get("plugin:administration", "password")
 
 
 admin = flask.Blueprint("administration", __name__,
@@ -21,9 +32,17 @@ admin = flask.Blueprint("administration", __name__,
 def verify_authentication():
     if "/static/" in flask.request.path or flask.request.path == flask.url_for("administration.login"):
         return
-    if not flask.session.get("logged-in"):
-        return flask.redirect(flask.url_for(".login", back_to=flask.request.path))
-    flask.g.user = "<admin>"
+
+    authorization = flask.request.authorization
+    if not (authorization and is_valid_auth(authorization.username, authorization.password))\
+            and "logged-in" not in flask.session:
+        if "/api/" in flask.request.path or flask.request.path == flask.url_for(".get_logs"):
+            raise flask.abort(http.HTTPStatus.UNAUTHORIZED)
+        else:
+            return flask.redirect(flask.url_for(".login", back_to=flask.request.path))
+
+    if not hasattr(flask.g, "user"):
+        flask.g.user = "<admin>"
 
 
 @admin.get("/")
@@ -46,20 +65,50 @@ def audit_log():
     return flask.render_template("audit_log.html")
 
 
+def is_valid_auth(username: str, password: str):
+    r"""TODO: also allow api-keys"""
+
+    # Login over config-file
+    right_username = hmac.compare_digest(username, ADMIN_USERNAME)
+    right_password = hmac.compare_digest(password, ADMIN_PASSWORD)
+    if right_username and right_password:
+        flask.g.user = "<admin>"
+        return True
+    
+    # Login over user with 'admin'-role
+    with flask.g.db_conn as conn:
+        cursor = conn.cursor()
+        table = Table(USERS_TABLENAME)
+        query = Query \
+            .from_(Schema(USERS_SCHEMANAME).__getattr__(USERS_TABLENAME)) \
+            .select('*') \
+            .where(table.username == username)
+        cursor.execute(str(query))
+        row = cursor.fetchone()
+        if not row:
+            return False
+        flask.g.roles = row.roles
+        if not compare_password_to_hash(password.encode(), base64.b64decode(row.passwordhash.encode())):
+            return False
+        if "admin" in row.roles:
+            flask.g.user = row.username
+            return True
+    return False
+
+
 @admin.route("/login", methods=["GET", "POST"])
 def login(back_to: str = None):
     back_to = back_to or flask.url_for(".index")
     if flask.request.method == "POST":
-        uname = flask.request.form.get("username")
-        if uname is None:
+        username = flask.request.form.get("username")
+        if username is None:
             return flask.render_template("login.html", back_to=back_to, error="Missing username in form")
-        pword = flask.request.form.get("password")
-        if pword is None:
+        password = flask.request.form.get("password")
+        if password is None:
             return flask.render_template("login.html", back_to=back_to, error="Missing password in form")
-        right_username = hmac.compare_digest(uname, "admin")
-        right_password = hmac.compare_digest(pword, "admin")
-        if right_username and right_password:
+        if is_valid_auth(username=username, password=password):
             flask.session["logged-in"] = True
+            flask.session["current-admin"] = username
             return flask.redirect(back_to)
         else:
             return flask.render_template("login.html", back_to=back_to, error="Invalid username or password")
